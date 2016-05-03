e251bffd731aa08800a5ef95a48de96304cae697
elfrevaldes <elfre.valdes@gmail.com>
2016-05-02 18:34:00 -0600

    changes to assignment and css so that mobile will work

--- js/accordion.js
+++ js/accordion.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     // makes the accordion work
     $(function () {
-        $("#accordion").accordion({ header: "h3" });//, collapsible: true, active: true });
+        $("#accordion").accordion({ header: "h3", collapsible: true, active: true });
     });
     $(function () {
         $("#tabs").tabs();
