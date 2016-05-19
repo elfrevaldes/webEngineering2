a269b1239b483ba7af0c76c76c75f8ff9688df3c
elfrevaldes <elfre.valdes@gmail.com>
2016-05-19 11:48:55 -0600

    new folder and some changes

---
+++ process/testconnection.php
@@ -0,0 +1,16 @@
+﻿<?php
+	$dbUser = "adminWekFWSb";
+	$dbPassword = "gVLBUqSmwDkf"; 
+	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n"; 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+	<meta charset="utf-8" />
+</head>
+<body>
+
+</body>
+</html>
