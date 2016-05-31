64d416c20127c770d484b148bd10d8c3c2efcd09
elfrevaldes <elfre.valdes@gmail.com>
2016-05-31 12:16:32 -0600

    new index.php without the php part

--- index.php
+++ index.php
@@ -15,7 +15,78 @@
     <link rel="stylesheet" href="css/mobile.css" type="text/css" media="screen and (max-width:600px)" />
     <link rel="stylesheet" href="css/desktop.css" type="text/css" media="screen and (min-width: 600px)" />
 </head>
-   <body>
+<body>
+	<img id="hfsbg" src="https://c7.staticflickr.com/9/8239/8543660542_bc565a3ae4_k.jpg" width="1980" height="1360" />
+    <div id="header">
+        <span class="icoMenu"></span>
+        <nav class="mainNav">
+            <ul>
+                <li><a href="#" title="Projects">Projects</a></li>
+                <li><a href="assign/index.html" title="Assignments">Assignments</a></li>
+                <li><a href="#" title="Contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div id="body">
+        <div id="tabs">
+            <ul>
+                <li><span id="loginLink" class="active link" onclick="openTab('login-tab');">Login</span></li>
+                <li><span id="signupLink" class="inactive link" onclick="openTab('signup-tab');">Sign up</span></li>
+            </ul>
+            <div id="login-tab">
+							<label id='loginLbl'></label>
+                <form action="" method="POST" onsubmit="return fullValidation('login');">
+                    <p>
+                        <input id="email" name="email" type="text" placeholder="Email" onchange="isEmailValid('#loginLbl', '#email');" /><b>*</b>
+                    </p>
+                    <p>
+                        <input id="password" name="password" type="password" placeholder="Password" onchange="isPassValid('#loginLbl', '#password');" /><b>*</b>
+                    </p>
+                    <p><input id="loginBtn" name="loginBtn" type="submit" value="Login" /></p>
+                </form>
+            </div>
+            <div id="signup-tab">
+							<label id='signupLbl'>
+							</label>
+                <form action="" method="POST" onsubmit="return (fullValidation('signup'));">
+                    <p><input id="name" name="name" type="text" placeholder="Name" onchange="isValidName('#signupLbl', '#name');" /><b>*</b></p>
+										<p><input id="last" name="last" type="text" placeholder="Last Name" onchange="isValidName('#signupLbl', '#last');" /><b>*</b></p>
+                    <p><input id="semail" name="semail" type="text" placeholder="Email" onchange="isEmailValid('#signupLbl', '#semail');" /><b>*</b></p>
+                    <p><input id="ward" name="ward" type="text" placeholder="Add new Ward" onchange="isValidWard();" style="display: none;"/>
+										<select id="ward_name" onchange="addNewWard();">
+											<option value='blank' disabled="true" selected></option>
+											<?php
+											try{
+												//$query = 'SELECT * FROM ward';
+											   //$stmt = $db->prepare($query);
+											   //$stmt->execute();
 
-   </body>
+                                    //$wards = $stmt->fetchAll(PDO::FETCH_ASSOC); // key value pairs
+												//foreach ($wards as $ward) {
+												//		$name = $ward['name'];
+										      //	   $id = $ward['id'];
+												//		echo "<option value='$id'>$name</option>";
+												//	}
+												echo "<option value='newWard'>Add New Ward</option>";
+											}
+											catch(Exception $ex)
+											{
+												 echo 'Error while requesting wards.';
+											}
+											?>
+										</select><b>*</b>
+										</p>
+                    <p>
+                        <input id="spassword" name="spassword" type="password" placeholder="Password" onchange="isPassValid('#signupLbl', '#spassword');" />
+                        <input id="srpassword" name="srpassword" type="password" placeholder="Repeat Password" onchange="isPassValid('#signupLbl', '#srpassword');" /> <b>*</b>
+                    </p>
+                    <p><input id="signupBtn" name="signupBtn" type="submit" value="Sign up" /></p>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- end of the body -->
+    <div class="clearfooter"></div>
+    <div id="footer"><h3>There will be something here in future updates</h3></div>
+</body>
 </html>
