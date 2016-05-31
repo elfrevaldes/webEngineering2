73d74f23f111c9910461355101a0dbcd3920f9d1
elfrevaldes <elfre.valdes@gmail.com>
2016-05-31 12:04:56 -0600

    trying to fix the index.php

--- index.php
+++ index.php
@@ -1,115 +1,115 @@
 ﻿<?php
-	session_start();
-	 if (isset($_SESSION["email"]))
-		header('Location: process/welcome.php');
-	// the connection
-	// we need the access to the db but only one time
-	include_once('process/database.php');
-	$db = dbConnect();
+	// session_start();
+	//  if (isset($_SESSION["email"]))
+	// 	header('Location: process/welcome.php');
+	// // the connection
+	// // we need the access to the db but only one time
+	// include_once('process/database.php');
+	// $db = dbConnect();
+	//
+	// if(isset($userExist))
+	// 	unset($_POST);
 
-	//if(isset($userExist))
-	//	unset($_POST);
-
-	if(isset($_POST['loginBtn']))
-	{
-		  include_once('process/settings.php');
-		  // security
-		  $email = sanitizeInput($_POST['email']);
-		  $pass = sanitizeInput($_POST['password']);
-		  // checking if the user privided exist
-			try
-			{
-					$query = 'SELECT id, display_name, pass, email, ward_id FROM user WHERE email=:email';
-		      $statement = $db->prepare($query);
-					$statement->bindValue(':email', $email, PDO::PARAM_STR);
-		      $result = $statement->execute();
-					// requesting the user
-					$user = $statement->fetch();
-		      if ($user)
-		      {
-						// checking that password match
-		        if ($pass == $user[2])
-		        {
-							$_SESSION["id"] = $user[0];
-		          $_SESSION["display_name"] = $user[1];
-		          $_SESSION["email"] = $user[3];
-		          $_SESSION["ward_id"] = $user[4];
-
-
-		          // finding the ward name
-		          $que = 'SELECT name FROM ward WHERE id = :id';
-		          $sta = $db->prepare($que);
-		          $sta->bindValue(':id', $user[4], PDO::PARAM_INT);
-		          $ward = $sta->execute();
-		          $wardName = $sta->fetch();
-
-		          $_SESSION["ward_name"] = $wardName[0];
-
-		          header("Location: process/welcome.php");
-		      	}
-					}
-					//else{
-						// I dont know how to handle this part
-
-					//}
-		      // I need to count the amount of times the user tries to access
-		    //} // there is no such user.
-		    //else
-		    //{
-		      // setting the the post variable on to display error message
-		      //$_SESSION['pass_failed'] = "yes";
-					// user enter wrong password
-		    //}
-	   }
-	   catch(Exception $ex)
-	   {
-	      echo 'Error while loging in.';
-	   }
- }
-
- //if(isset($_POST['signup']))
- if (isset($_POST['signupBtn']))
- {
-		 echo "<br /> HERE";
-		 	 //echo "<br />sending info: $_POST['semail']";
-			 include_once('process/settings.php');
-			 // security
-			 $semail = sanitizeInput($_POST['semail']);
-			 try
-			 {
-				 $query = 'SELECT * FROM user WHERE email=:email';
-				 $statement = $db->prepare($query);
-				 $statement->bindValue(':email', $semail, PDO::PARAM_STR);
-				 $result = $statement->execute();
-				 $result = $statement->fetch();
-
-				 if (!$result)
-				 {
-					//$_SESSION['name'] = sanitizeInput($_POST["name"]);
-				  //$_SESSION['last'] = sanitizeInput($_POST["last"]);
-					$_SESSION['display_name'] = sanitizeInput($_POST["name"]) .' '. sanitizeInput($_POST["last"]);
-				  $_SESSION['email'] = sanitizeInput($_POST["semail"]);
-					$_SESSION['pass'] = sanitizeInput($_POST["spassword"]);
-				  $_SESSION['ward_name'] = sanitizeInput($_POST["ward"]);
-
-					header("Location: process/process.php");
-				 }
-				 // I need to count the amount of times the user tries to access
-				 // there is no such user.
-				 else
-				 {
-					 echo "<br />User " . $semail . " already in use";
-					// echo "<br />email found $email";
-					 // setting the the post variable on to display error message
-					 // $_SESSION["user_already"] = $email;
-					 //$userExist = "test@";
-				 }
-		}
-		catch(Exception $ex)
-		{
-			 echo 'Error while loging in.';
-		}
-}
+// 	if(isset($_POST['loginBtn']))
+// 	{
+// 		  include_once('process/settings.php');
+// 		  // security
+// 		  $email = sanitizeInput($_POST['email']);
+// 		  $pass = sanitizeInput($_POST['password']);
+// 		  // checking if the user privided exist
+// 			try
+// 			{
+// 					$query = 'SELECT id, display_name, pass, email, ward_id FROM user WHERE email=:email';
+// 		      $statement = $db->prepare($query);
+// 					$statement->bindValue(':email', $email, PDO::PARAM_STR);
+// 		      $result = $statement->execute();
+// 					// requesting the user
+// 					$user = $statement->fetch();
+// 		      if ($user)
+// 		      {
+// 						// checking that password match
+// 		        if ($pass == $user[2])
+// 		        {
+// 							$_SESSION["id"] = $user[0];
+// 		          $_SESSION["display_name"] = $user[1];
+// 		          $_SESSION["email"] = $user[3];
+// 		          $_SESSION["ward_id"] = $user[4];
+//
+//
+// 		          // finding the ward name
+// 		          $que = 'SELECT name FROM ward WHERE id = :id';
+// 		          $sta = $db->prepare($que);
+// 		          $sta->bindValue(':id', $user[4], PDO::PARAM_INT);
+// 		          $ward = $sta->execute();
+// 		          $wardName = $sta->fetch();
+//
+// 		          $_SESSION["ward_name"] = $wardName[0];
+//
+// 		          header("Location: process/welcome.php");
+// 		      	}
+// 					}
+// 					//else{
+// 						// I dont know how to handle this part
+//
+// 					//}
+// 		      // I need to count the amount of times the user tries to access
+// 		    //} // there is no such user.
+// 		    //else
+// 		    //{
+// 		      // setting the the post variable on to display error message
+// 		      //$_SESSION['pass_failed'] = "yes";
+// 					// user enter wrong password
+// 		    //}
+// 	   }
+// 	   catch(Exception $ex)
+// 	   {
+// 	      echo 'Error while loging in.';
+// 	   }
+//  }
+//
+//  //if(isset($_POST['signup']))
+//  if (isset($_POST['signupBtn']))
+//  {
+// 		 echo "<br /> HERE";
+// 		 	 //echo "<br />sending info: $_POST['semail']";
+// 			 include_once('process/settings.php');
+// 			 // security
+// 			 $semail = sanitizeInput($_POST['semail']);
+// 			 try
+// 			 {
+// 				 $query = 'SELECT * FROM user WHERE email=:email';
+// 				 $statement = $db->prepare($query);
+// 				 $statement->bindValue(':email', $semail, PDO::PARAM_STR);
+// 				 $result = $statement->execute();
+// 				 $result = $statement->fetch();
+//
+// 				 if (!$result)
+// 				 {
+// 					//$_SESSION['name'] = sanitizeInput($_POST["name"]);
+// 				  //$_SESSION['last'] = sanitizeInput($_POST["last"]);
+// 					$_SESSION['display_name'] = sanitizeInput($_POST["name"]) .' '. sanitizeInput($_POST["last"]);
+// 				  $_SESSION['email'] = sanitizeInput($_POST["semail"]);
+// 					$_SESSION['pass'] = sanitizeInput($_POST["spassword"]);
+// 				  $_SESSION['ward_name'] = sanitizeInput($_POST["ward"]);
+//
+// 					header("Location: process/process.php");
+// 				 }
+// 				 // I need to count the amount of times the user tries to access
+// 				 // there is no such user.
+// 				 else
+// 				 {
+// 					 echo "<br />User " . $semail . " already in use";
+// 					// echo "<br />email found $email";
+// 					 // setting the the post variable on to display error message
+// 					 // $_SESSION["user_already"] = $email;
+// 					 //$userExist = "test@";
+// 				 }
+// 		}
+// 		catch(Exception $ex)
+// 		{
+// 			 echo 'Error while loging in.';
+// 		}
+// }
 ?>
 <!DOCTYPE html>
 <html>
